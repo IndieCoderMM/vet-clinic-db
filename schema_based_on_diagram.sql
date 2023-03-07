fb7b6612c7414b38184362c3f596375405f3f85b
Abel Seifu <abel.workmail@gmail.com>
2023-03-07 12:04:09 +0300

    Add treatments table and many to many relationship

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,4 +16,22 @@ CREATE TABLE medical_histories (
     status varchar(100),
     PRIMARY KEY(id),
     FOREIGN KEY(patient_id) REFERENCES patients(id)
-)
+);
+
+-- Table for Treatments
+
+CREATE TABLE treatments (
+    id INT PRIMARY KEY,
+    type VARCHAR,
+    name VARCHAR
+);
+
+-- Many to many relationship table
+
+CREATE TABLE medical_histories_treatments (
+    id INT PRIMARY KEY,
+    medical_history_id INT NOT NULL,
+    treatment_id INT NOT NULL,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
