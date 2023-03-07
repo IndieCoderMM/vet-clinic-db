7f5f66d8f49b1d9a0dda1ddc1ae440477cf9c442
heinthant <hthant00chk@gmail.com>
2023-03-07 15:45:51 +0630

    Add tables for invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -35,3 +35,33 @@ CREATE TABLE medical_histories_treatments (
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
     FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
+
+-- Table for invoices 
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id),
+    PRIMARY KEY (id)
+);
+
+-- Table for invoice_items
+
+CREATE TABLE invoice_items (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id),
+    PRIMARY KEY (id)
+);
+
+-- Indexing foreign keys
+
+CREATE INDEX medical_history_id_idx ON invoices(medical_history_id);
+CREATE INDEX invoice_id_idx ON invoice_items(invoice_id);
+CREATE INDEX treatment_id_idx ON invoice_items(treatment_id);
+CREATE INDEX patient_id_idx ON medical_histories(patient_id);
